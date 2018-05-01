55360824c186cfafcdb9b8e7be2e9a4e058e681b
Joao Maurício <joao.carvalho@ccc.ufcg.edu.br>
2018-05-01 18:03:27 -0300

    Implemented Trie search and insert

--- Trie.py
+++ Trie.py
@@ -1,6 +1,28 @@
 class Node:
 	def __init__(self, endOfWord = False):
 		self.children = {}
-		self.endOfWord = endOfWord
+		self.end_of_word = endOfWord
 
 class Trie:
+	def __init__(self):
+		self.root = Node()
+
+	def insert(self, word):
+		current = self.root
+		for i in word:
+			if i in current.children:
+				current = current.children[i]
+			else:
+				current[i] = Node()
+				current = curret[i]
+		current.end_of_word = True
+
+	def search(self, word):
+		current = self.root
+		for i in word:
+			if i in current.children:
+				current = current.children[i]
+			else:
+				return False
+		
+		return current.end_of_word
